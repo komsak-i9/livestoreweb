2562e35671cf025edac08de27a1473fccfc5d507
ARIT <ARIT@ARIT-5Y5ZPR1.arit.co.th>
2017-07-21 11:59:10 +0700

    Fix Datasource

--- src/main/java/com/example/BasicApplication.java
+++ src/main/java/com/example/BasicApplication.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan
 public class BasicApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/livestore/ItemDAOMySQL.java
+++ src/main/java/com/example/livestore/ItemDAOMySQL.java
@@ -1,14 +1,11 @@
 package com.example.livestore;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.RowMapper;
